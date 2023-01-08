964e17e093f30860d5c62c91efd94f37cb9efee7
Toluwaa-o <121364075+Toluwaa-o@users.noreply.github.com>
2023-01-08 14:32:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var acc = document.getElementsByClassName("accordian");
 var para = document.getElementsByClassName("accordian-content");
+var bold = document.querySelectorAll("h2");
 for(var i=0; i<acc.length; i++){
   (function(i) {
     acc[i].addEventListener("click", function(){
       this.classList.toggle("button-pressed");
       para[i].style.display = para[i].style.display === 'block' ? 'none' : 'block';
+      bold[i].style.fontWeight = bold[i].style.fontWeight === '700' ? '400' : '700';
     });
   })(i);
 }
